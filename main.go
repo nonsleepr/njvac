57da70197762e4fe0a1d535d07f09a7b1f6731e5
Alexander Bessonov <abessonov2@bloomberg.net>
2021-02-16 14:34:38 -0500

    Refactor CVS into package

--- main.go
+++ main.go
@@ -1,49 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/nonsleepr/go-nj-vaccine/src"
 	"github.com/tomlazar/table"
-	"io/ioutil"
-	"net/http"
 	"os"
 )
 
-const REFERER = "https://www.cvs.com/immunizations/covid-19-vaccine"
-const STATUS_URL = "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.NJ.json"
-
-type CVSAvailability struct {
-	Available    int    `table:"TOTAL AVAILABLE" json:"totalAvailable,string"`
-	City         string `table:"CITY"`
-	State        string `table:"STATE"`
-	PctAvailable string `table:"PERCENT AVAILABLE"`
-	Status       string `table:"STATUS"`
-}
-
-type CVSResult struct {
-	Payload struct {
-		CurrentTime string
-		Data        map[string][]CVSAvailability
-	} `json:"responsePayloadData"`
-	Metadata struct {
-		Status string `json:"statusDesc"`
-	} `json:"responseMetaData"`
-}
-
 func main() {
-	req, _ := http.NewRequest("GET", STATUS_URL, nil)
-	req.Header.Add("Referer", REFERER)
-	resp, err := http.DefaultClient.Do(req)
+	cvsData, cvsTimestamp, err := njvac.GetCVSData()
 	if err != nil {
 		return
 	}
-	defer resp.Body.Close()
-	data, _ := ioutil.ReadAll(resp.Body)
-	var result CVSResult
-	json.Unmarshal(data, &result)
-
-	if result.Metadata.Status == "Success" {
-		fmt.Printf("Data as of %s\n", result.Payload.CurrentTime)
-		table.MarshalTo(os.Stdout, result.Payload.Data["NJ"], &table.Config{})
-	}
+	fmt.Printf("Data as of %s\n", cvsTimestamp)
+	table.MarshalTo(os.Stdout, cvsData, &table.Config{})
 }
---
+++ src/cvs.go
@@ -0,0 +1,54 @@
+package njvac
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const referer = "https://www.cvs.com/immunizations/covid-19-vaccine"
+const statusUrl = "https://www.cvs.com/immunizations/covid-19-vaccine.vaccine-status.NJ.json"
+
+type CVSAvailability struct {
+	Available    int    `table:"TOTAL AVAILABLE" json:"totalAvailable,string"`
+	City         string `table:"CITY"`
+	State        string `table:"STATE"`
+	PctAvailable string `table:"PERCENT AVAILABLE"`
+	Status       string `table:"STATUS"`
+}
+
+type CVSResult struct {
+	Payload struct {
+		CurrentTime string
+		Data        map[string][]CVSAvailability
+	} `json:"responsePayloadData"`
+	Metadata struct {
+		Status string `json:"statusDesc"`
+	} `json:"responseMetaData"`
+}
+
+func GetCVSData() ([]CVSAvailability, string, error) {
+	req, err := http.NewRequest("GET", statusUrl, nil)
+	if err != nil {
+		return nil, "", err
+	}
+	req.Header.Add("Referer", referer)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, "", err
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", err
+	}
+	var result CVSResult
+	json.Unmarshal(data, &result)
+
+	if result.Metadata.Status == "Success" {
+		return result.Payload.Data["NJ"], result.Payload.CurrentTime, nil
+	} else {
+		return nil, "", fmt.Errorf(result.Metadata.Status)
+	}
+}
